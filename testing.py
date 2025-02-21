c6b48548820d6008dd02489485df27aab2ac39fc
Tahsin Rahman <tr174@duke.edu>
2025-02-20 19:01:23 -0500

    testing new settings

--- testing.py
+++ testing.py
@@ -1,16 +1,25 @@
 """
 Simple Flight Dynamics Model (FDM) example that makes the altitude increase and the plane roll in the air.
 """
+import math
 import time
-from flightgear_python.fg_if import FDMConnection
+from flightgear_python.fg_if import FDMConnection =
 
 def fdm_callback(fdm_data, event_pipe):
-    if event_pipe.child_poll():
-        phi_rad_child, = event_pipe.child_recv()  # unpack tuple
-        # set only the data that we need to
-        fdm_data['phi_rad'] = phi_rad_child  # we can force our own values
-    fdm_data.alt_m = fdm_data.alt_m + 0.5  # or just make a relative change
-    return fdm_data  # return the whole structure
+    fdm_data.alt_m = fdm_data.alt_m + 1.25
+    fdm_data.phi_rad = fdm_data.phi_rad + 0.001
+    fdm_data_psi_rad = fdm_data.psi_rad = 0.01
+    fdm_data.theta_rad = fdm_data.theta_rad + 0.001
+    fdm_data.lat_rad = fdm_data.lat_rad + 1e-6
+    fdm_data.lon_rad = fdm_data.lon_rad + 1e-6
+    fdm_data.elevator = math.sin(time.time())
+    fdm_data.left_aileron = math.sin(time.time())
+    fdm_data.rudder = math.sin(time, time())
+    global i
+    i+=1
+    print(i)
+
+    return fdm_data  
 
 """
 Start FlightGear with `--native-fdm=socket,out,30,localhost,5501,udp --native-fdm=socket,in,30,localhost,5502,udp`
@@ -28,4 +37,4 @@ if __name__ == '__main__':  # NOTE: This is REQUIRED on Windows!
         phi_rad_parent += 0.1
         # could also do `fdm_conn.event_pipe.parent_send` so you just need to pass around `fdm_conn`
         fdm_event_pipe.parent_send((phi_rad_parent,))  # send tuple
-        time.sleep(0.5)
+        time.sleep(0.01)
